0ce6e6e67867358bae79163b04132f85d2b0b06c
Léopold Mebazaa <lemeb@users.noreply.github.com>
2025-06-12 19:37:04 +0200

    feat: add python backend (just the beginning)

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,38 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  rewrites: async () => {
+    return [
+      // {
+      //   source: '/api/:path*',
+      //   destination:
+      //     process.env.NODE_ENV === 'development'
+      //       ? 'http://127.0.0.1:8000/api/:path*'
+      //       : '/api/',
+      // },
+      {
+        source: '/api/chat',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:8000/api/chat'
+            : '/api/chat',
+      },
+      {
+        source: '/docs',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:8000/docs'
+            : '/api/docs',
+      },
+      {
+        source: '/openapi.json',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:8000/openapi.json'
+            : '/api/openapi.json',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
